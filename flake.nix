fd5f71c4ad5fbe25705438a210578ad17f8246ef
qbisi <qbisicwate@gmail.com>
2024-10-18 18:41:55 +0800

    fix

--- flake.nix
+++ flake.nix
@@ -8,6 +8,8 @@
   };
 
   nixConfig = {
+    # extra-sandbox-paths = [ "/sys/devices/system/cpu/cpu0/topology" ];
+    # extra-sandbox-paths = [ "/sys/devices/system/cpu" ];
     extra-sandbox-paths = [ "/sys" ];
   };
 
@@ -24,14 +26,41 @@
         packages = rec {
           default = hello;
 
+          mpiBwrapped = with pkgs; stdenv.mkDerivation {
+            name = "mpi-bwrapped";
+
+            phases = [ "buildPhase" ];
+
+            nativeBuildInputs = [ makeShellWrapper ];
+
+            buildInputs = [ mpi bubblewrap ];
+
+            buildPhase = ''
+              install -d $out/root/{proc,dev,build,nix/store,sys/class/net}
+              for i in {0..1}
+              do
+              install -d $out/root/sys/devices/system/cpu/cpu$i/topology
+              echo $i > $out/root/sys/devices/system/cpu/cpu$i/topology/core_cpus
+              done
+              echo '0-1' > $out/root/sys/devices/system/cpu/possible
+              # echo '0-1' > $out/root/sys/devices/system/cpu/online
+              # echo '0-1' > $out/root/sys/devices/system/cpu/present
+              makeWrapper "${bubblewrap}/bin/bwrap" "$out/bin/mpirun" --add-flags "--bind $out/root / --bind /build /build --proc /proc --dev /dev --bind /nix/store /nix/store ${mpi}/bin/mpirun --prefix ${mpi}"
+            '';
+          };
+
           hello = with pkgs; stdenv.mkDerivation {
             name = "hello-mpi";
 
-            src = lib.fileset.toSource { root = ./.; fileset = ./.; };
+            src = lib.fileset.toSource { root = ./hello; fileset = ./hello; };
+
+            # nativeBuildInputs = [ mpiBwrapped ];
 
             buildInputs = [ mpi ];
 
             doCheck = true;
+
+            # nativeCheckInputs = [ mpiBwrapped ];
 
             installFlags = [ "PREFIX=\${out}" ];
           };
